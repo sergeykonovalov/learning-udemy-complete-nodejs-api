13276f42be86452aededaf1b6596f99878499efe
Sergey Konovalov (MBP) <skonovalov@gmail.com>
2018-04-21 14:46:41 +0300

    Rename playground files

---
+++ api/playground/mongoose-delete.js
@@ -0,0 +1,16 @@
+const { mongoose } = require('./../db/mongoose');
+const { Todo } = require('./../models/todo');
+const { ObjectID } = require('mongodb');
+
+Todo.remove({}).then((result) => {
+  console.log(result);
+})
+
+// Will delete and also return the document with removed data
+Todo.findOneAndRemove.({}).then((result) => {
+  console.log(result);
+});
+
+Todo.findByIdAndRemove(id).then((todo) => {
+  console.log(result);
+});
