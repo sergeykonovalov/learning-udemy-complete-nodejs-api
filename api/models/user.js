97ff3dbf2e8129f5f78d45211f74a2cfb6a9b51e
Sergey Konovalov (MBP) <skonovalov@gmail.com>
2018-04-22 15:03:25 +0300

    Add troubleshoot logging and resolved issue

--- api/models/user.js
+++ api/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
 let UserSchema = new mongoose.Schema({
   email: {
@@ -9,6 +10,7 @@ let UserSchema = new mongoose.Schema({
     minlength: 1,
     unique: true,
     validate: {
+      isAsync: true,
       validator: validator.isEmail,
       message: '{VALUE} is not a valid email'
     }
@@ -29,6 +31,29 @@ tokens: [{
   }
 }]
 });
+
+// We will use `function` to bind to `this`
+UserSchema.methods.generateAuthToken = function () {
+  let user = this;
+  console.log('=== MODEL - Original User ===\n', user);
+  let access = 'auth';
+  let token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
+  console.log('=== MODEL - Original User Tokens ===\n', user.tokens);
+  console.log('=== MODEL - Signed Token ===\n', token);
+  try { 
+    user.tokens = user.tokens.concat([{ access, token }]);
+  } catch(e) {
+    console.log('=== MODEL - Error With Array ===\n', e);
+  }
+  console.log('=== MODEL - Updated User Tokens ===\n', user.tokens);
+  console.log('=== MODEL - Updated User Model ===\n', user);
+  return user.save().then(() => {
+    console.log('=== MODEL - Token We Will Now Return ===\n', token);
+    return token;
+  }).catch((e) => {
+    console.log('=== MODEL - Error On Model Save ===\n', e);
+  });
+};
 
 let User = mongoose.model('User', UserSchema);
 
