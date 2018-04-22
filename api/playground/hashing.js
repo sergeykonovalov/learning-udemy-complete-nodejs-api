174450f7c2f23f24085c759443ce7ae49d96439b
Sergey Konovalov (MBP) <skonovalov@gmail.com>
2018-04-22 12:34:32 +0300

    Add example of hashing

---
+++ api/playground/hashing.js
@@ -0,0 +1,22 @@
+const { SHA256 } = require('crypto-js');
+let message = 'I am user number 4';
+let hash = SHA256(message).toString();
+console.log(`Message: ${message}`);
+console.log(`Message hash: ${hash}`);
+
+let data = {
+    id: 4
+};
+
+let token = {
+    data,
+    hash: SHA256(JSON.stringify(data) + 'some secret salt').toString()
+};
+
+let resultHash = SHA256(JSON.stringify(token.data) + 'some secret salt').toString();
+
+if (resultHash === token.hash) {
+    console.log('data was not changed');
+} else {
+    console.log('do not trust this data');
+}
